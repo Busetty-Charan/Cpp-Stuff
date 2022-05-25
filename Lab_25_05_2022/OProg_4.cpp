ef7885321d6337e4f0e412009ca84a57df31ac59
Busetty Charan <94380841+Busetty-Charan@users.noreply.github.com>
2022-05-25 05:09:23 +0000

    just something cringe

--- Lab_25_05_2022/OProg_4.cpp
+++ Lab_25_05_2022/OProg_4.cpp
@@ -3,20 +3,20 @@ using namespace std;
  
 class Complex
 {
-private:
-
-    double real;
-    double imag;
- 
-public:
-   
-    explicit Complex(double r = 0.0, double i = 0.0):real(r), imag(i)
-    {}
- 
-    bool operator == (Complex rhs)
-    {
-        return (real == rhs.real && imag == rhs.imag);
-    }
+    private:
+    
+        double real;
+        double imag;
+     
+    public:
+       
+        explicit Complex(double r = 0.0, double i = 0.0):real(r), imag(i)
+        {}
+     
+        bool operator == (Complex rhs)
+        {
+            return (real == rhs.real && imag == rhs.imag);
+        }
 };
  
 int main()
